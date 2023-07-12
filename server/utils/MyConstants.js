a418994638d1469a72530d4242ecc1262c4c9d21
hoagkiens02 <tangvannhan3679@gmail.com>
2023-07-12 09:36:35 +0700

    Shoppingonline

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'clusterroad.efeyq2z.mongodb.net',
+    DB_USER: 'FIRall',
+    DB_PASS: 'pwdTungAn',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'marinesama632@gmail.com',
+    EMAIL_PASS: 'fbkozkbwuffwdwxd',
+    JWT_SECRET: '012',
+    JWT_EXPIRES: '86400000', // in milliseconds
+  };
+  module.exports = MyConstants;
